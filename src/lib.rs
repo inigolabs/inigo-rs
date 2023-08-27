8c2ada448c0322900ab8217f87233bc623c298d6
Andrii Soluk <isoluchok@gmail.com>
2023-08-27 10:00:01 +0300

    lib: Add RouterService
    commit-id:f2343b4c

--- src/lib.rs
+++ src/lib.rs
@@ -16,13 +16,18 @@ use std::sync::{Arc, Mutex};
 use apollo_router::graphql;
 use apollo_router::layers::ServiceBuilderExt;
 use apollo_router::plugin::{Plugin, PluginInit};
+use apollo_router::services::router;
 use apollo_router::services::{subgraph, supergraph};
+use futures::future::BoxFuture;
 use http::{HeaderMap, HeaderValue};
 use libloading::{Library, Symbol};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use serde_json_bytes::{ByteString, Value};
-use tower::{BoxError, ServiceBuilder, ServiceExt};
+use std::task::Context;
+use std::task::Poll;
+use tower::buffer::Buffer;
+use tower::{BoxError, Service, ServiceBuilder, ServiceExt};
 
 #[repr(C)]
 pub struct SidecarConfig {
@@ -428,6 +433,11 @@ impl Plugin for Middleware {
         Ok(middleware)
     }
 
+    // NOTE: Uncomment it to enable RouterService
+    // fn router_service(&self, service: router::BoxService) -> router::BoxService {
+    //     RouterService::new(service).boxed()
+    // }
+
     fn subgraph_service(&self, _name: &str, service: subgraph::BoxService) -> subgraph::BoxService {
         if !self.enabled {
             return service;
@@ -537,6 +547,45 @@ impl Plugin for Middleware {
             .map_response(process_resp_fn(inigo))
             .service(service)
             .boxed()
+    }
+}
+
+#[derive(Clone)]
+pub(crate) struct RouterService {
+    service: Buffer<router::BoxService, router::Request>,
+}
+
+impl RouterService {
+    #[allow(dead_code)]
+    pub(crate) fn new(service: router::BoxService) -> Self {
+        Self {
+            service: ServiceBuilder::new().buffered().service(service),
+        }
+    }
+}
+
+impl Service<router::Request> for RouterService {
+    type Response = router::Response;
+    type Error = BoxError;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: router::Request) -> Self::Future {
+        let clone = self.service.clone();
+        let mut inner = std::mem::replace(&mut self.service, clone);
+
+        let fut = async move {
+            let mut res = inner.call(req).await?;
+            let (parts, res_body) = res.response.into_parts();
+            let original_res_body = hyper::body::to_bytes(res_body).await?;
+            res.response = http::Response::from_parts(parts, hyper::Body::from(original_res_body));
+            Ok(res)
+        };
+
+        Box::pin(fut)
     }
 }
 
