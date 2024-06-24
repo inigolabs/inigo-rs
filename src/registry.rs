e2df3ab5af990d68a699acf5d335544643b4209e
Nikolai Kaploniuk <nkaploniuk@inigo.io>
2024-06-24 13:36:07 -0700

    update apollo-router version
    commit-id:b80e5122

--- src/registry.rs
+++ src/registry.rs
@@ -2,7 +2,7 @@
 MIT License
 
 Copyright (c) 2022 The Guild
-Copyright (c) 2023 Inigo
+Copyright (c) 2024 Inigo
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
